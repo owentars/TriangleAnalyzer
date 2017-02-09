446c664abb5ea47ebd3c764699d532838afa5e9d
Owen Tarswell <owentarswell@hotmail.com>
2017-02-09 16:33:58 -0500

    StartMenu method and TriangleDimensions method created

--- OTAssignment2/Program.cs
+++ OTAssignment2/Program.cs
@@ -10,6 +10,34 @@ namespace OTAssignment2
     {
         static void Main(string[] args)
         {
+            StartMenu();
+        }
+
+        private static void StartMenu()
+        {
+            string userInput = "";
+            while (userInput != "2")
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1. Enter triangle dimensions");
+                Console.WriteLine("2. Exit");
+                Console.Write("Make your selection now: ");
+                userInput = Console.ReadLine();
+                if (userInput == "1")
+                {
+                    triangleDimensions();
+                }
+            }
+        }
+
+        private static void triangleDimensions()
+        {
+            Console.Write("Input first number: ");
+            string a = Console.ReadLine();
+            Console.Write("Input second number: ");
+            string b = Console.ReadLine();
+            Console.Write("Input third number: ");
+            string c = Console.ReadLine();
         }
     }
 }
